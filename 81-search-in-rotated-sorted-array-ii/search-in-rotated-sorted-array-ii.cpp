06c9f7809a087785730a7c84f10dc81c64098bbc
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-12-19 22:24:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (89.71%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1;
+        int l = 0, r = nums.size() - 1;
 
-        while (low <= high) {
-            int mid = (high - low)/2 + low;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return true;
 
-            if (nums[mid] == target) return true;
-            else if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++;
-                high--;
-                continue;
-            }
-            else if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && nums[mid] >= target) {
-                    high = mid - 1;
-                }
-                else {
-                    low = mid + 1;
-                }
-            }
-            else {
-                if (nums[mid] <= target && nums[high] >= target) {
-                    low = mid + 1;
-                }
-                else {
-                    high = mid - 1;
-                }
+            // shrink duplicates aggressively
+            while (l < m && nums[l] == nums[m]) l++;
+            while (m < r && nums[r] == nums[m]) r--;
+
+            if (nums[l] <= nums[m]) { // left sorted
+                if (nums[l] <= target && target < nums[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            } else { // right sorted
+                if (nums[m] < target && target <= nums[r])
+                    l = m + 1;
+                else
+                    r = m - 1;
             }
         }
         return false;
     }
-}; 
+};
