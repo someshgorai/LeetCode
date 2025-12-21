d25b128c252a5e5310c9b078bbd64781eaa7ad06
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-12-21 21:38:30 +0530

    Time: 15 ms (20.43%) | Memory: 23 MB (43.72%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    long long hours(vector<int>& piles, int k) {
+        long long hours = 0;
+        for (int i=0; i<piles.size(); i++) {
+            hours += ceil ((double)piles[i]/ (double)k);
+        }
+        return hours;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        
+        while (low <= high) {
+            long long mid = (high+low)/2;
+            long long time = hours(piles, mid);
+            if (time <= h) {
+                high = mid -1;
+            } 
+            else {
+                low = mid +1;
+            }
+        }
+        return low;
+    }
+};
