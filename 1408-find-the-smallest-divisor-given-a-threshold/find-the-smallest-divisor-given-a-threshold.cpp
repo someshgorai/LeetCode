1329b34cb89bae8a79945e47c519abccb21d2186
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-12-22 12:21:31 +0530

    Time: 4 ms (84.66%) | Memory: 26.2 MB (46.64%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -9,19 +9,17 @@ private:
         return true;
     }
 public:
-    int smallestDivisor(vector<int>& nums, int threshold) {
-        int high = *max_element(nums.begin(), nums.end());
-        int low = 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (func(nums, mid, threshold)) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
+    int smallestDivisor(vector<int>& A, int threshold) {
+        int left = 1, right = 1e6, m, sum;
+        while (left < right) {
+            m = (left + right) / 2, sum = 0;
+            for (int i : A)
+                sum += (i + m - 1) / m;
+            if (sum > threshold)
+                left = m + 1;
+            else
+                right = m;
         }
-        return low;
+        return left;
     }
 };
