9bfb5276b7d3265bde97bb701dddc4b739911360
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-12-25 12:27:50 +0530

    Time: 354 ms (38.07%) | Memory: 308.4 MB (34.84%) - LeetSync

--- 4061-minimum-swaps-to-avoid-forbidden-values/minimum-swaps-to-avoid-forbidden-values.cpp
+++ 4061-minimum-swaps-to-avoid-forbidden-values/minimum-swaps-to-avoid-forbidden-values.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int minSwaps(vector<int>& a, vector<int>& f) {
-        int cnt = 0, n = a.size(), maxi = 0;
-        map<int,int> a_map, f_map, matching;
+        int n = a.size();
+        int cnt = 0, maxi = 0;
+
+        unordered_map<int,int> freqA, freqF, badFreq;
 
         for (int i = 0; i < n; i++) {
+            freqA[a[i]]++;
+            freqF[f[i]]++;
+
             if (a[i] == f[i]) {
-                cnt += (a[i] == f[i]);
-                matching[a[i]]++;
-                maxi = max(maxi, matching[a[i]]);
+                cnt++;
+                maxi = max(maxi, ++badFreq[a[i]]);
             }
-            a_map[a[i]]++; f_map[f[i]]++;
         }
 
-        for (auto& p : a_map) {
-            if (p.second > n - f_map[p.first]) return -1;
+        // Feasibility check
+        for (auto &p : freqA) {
+            int val = p.first;
+            if (p.second > n - freqF[val]) return -1;
         }
 
-        return max(maxi, (cnt + 1)/2);
+        return max(maxi, (cnt + 1) / 2);
     }
 };
