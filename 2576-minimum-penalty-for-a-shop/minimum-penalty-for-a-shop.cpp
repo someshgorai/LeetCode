b3fcf0e7c71f6fd20becb227a42100ce647ecc7e
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-12-26 11:51:18 +0530

    Time: 3 ms (89.07%) | Memory: 13.4 MB (67.65%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int penalty = 0;
-        for (char i:customers) {
-            if (i == 'Y') penalty++;
-        }
-        if (penalty == 0) return 0;
         int minPenalty = penalty, n = customers.size(), idx = -1;
         for (int i=0; i<n; i++) {
             if (customers[i] == 'Y') penalty--;
