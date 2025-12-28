7f526d76b33199cbd9b938dea147af498b76490a
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-12-28 20:59:19 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (87.46%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,15 +1,33 @@
 class Solution {
+private:
+    void placeNum(vector<int> &nums, int k) {
+        int low = 0, high = nums.size()-1;
+        while (low <= high) {
+            int mid = (high-low)/2 + low;
+            if (nums[mid] == k) return;
+            if (nums[mid] > k) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        nums[low] = k;
+        return;
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size(), maxLen = 1;
-        vector<int> len(n, 1);
-        for (int i=0; i<n; i++) {
-            for (int k=0; k<i; k++) {
-                if (nums[i] > nums[k]) {
-                    len[i] = max(1+len[k], len[i]);
-                }
+        vector<int> seq;
+        seq.push_back(nums[0]);
+        for (int i=1; i<n; i++) {
+            if (nums[i] > seq.back()) {
+                seq.push_back(nums[i]);
+                maxLen++;
             }
-            maxLen = max(maxLen, len[i]);
+            else {
+                placeNum(seq, nums[i]);
+            }
         }
         return maxLen;
     }
