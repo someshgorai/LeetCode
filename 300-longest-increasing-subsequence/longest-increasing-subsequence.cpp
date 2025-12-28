3051bae52a4e84635cf47789c6cdee6d5c499064
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-12-28 19:09:52 +0530

    Time: 59 ms (62.72%) | Memory: 14.4 MB (60.26%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,32 +1,16 @@
 class Solution {
-private:
-    int backtrack(int idx, int prev, vector<int> &nums, int n, vector<vector<int>> &dp) {
-        if (idx == n) return 0;
-
-        if (dp[idx][prev+1] != -1) return dp[idx][prev+1];
-        int take = 0;
-        if (prev == -1 || nums[idx] > nums[prev]) {
-            take = 1 + backtrack(idx+1, idx, nums, n, dp);
-        }
-        int notTake = 0 + backtrack(idx+1, prev, nums, n, dp);
-
-        return dp[idx][prev+1] = max(take, notTake);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n+1, 0);
-        for (int idx=n-1; idx>=0; idx--) {
-            for (int prev=idx-1; prev>=-1; prev--) {
-                int take = 0;
-                if (prev == -1 || nums[idx] > nums[prev]) {
-                    take = 1 + dp[idx+1];
+        int n = nums.size(), maxLen = 1;
+        vector<int> len(n, 1);
+        for (int i=0; i<n; i++) {
+            for (int k=0; k<i; k++) {
+                if (nums[i] > nums[k]) {
+                    len[i] = max(1+len[k], len[i]);
                 }
-                int notTake = 0 + dp[prev+1];
-
-                dp[prev+1] = max(take, notTake);                
             }
+            maxLen = max(maxLen, len[i]);
         }
-        return dp[-1+1];
+        return maxLen;
     }
 };
