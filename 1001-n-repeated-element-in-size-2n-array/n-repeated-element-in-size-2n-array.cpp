1478f7ef663598684ec7eb61174f1dc817fb789c
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-01-02 22:14:53 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (69.74%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < nums.size() - 2; i++) {
             if (nums[i] == nums[i+1] || nums[i] == nums[i+2])
                 return nums[i];
         }
-        return nums[n-1]; 
+        return nums.back(); 
     }
 };
