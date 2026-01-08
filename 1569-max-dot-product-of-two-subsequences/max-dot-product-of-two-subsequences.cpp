6d7afc8d7408df5ced1fb88d2e9ed44ba7190c91
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-01-08 22:23:20 +0530

    Time: 10 ms (89.20%) | Memory: 17.8 MB (54.80%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -20,7 +20,21 @@ private:
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int s1 = nums1.size(), s2 = nums2.size();
-        vector<vector<int>> dp(s1, vector<int> (s2, INT_MIN));
-        return backtrack(0, 0, nums1, nums2, nums1.size(), nums2.size(), dp);
+        vector<vector<int>> dp(s1+1, vector<int> (s2+1, INT_MIN));
+        for (int idx1=s1-1; idx1>=0; idx1--) {
+            for (int idx2=s2-1; idx2>=0; idx2--) {
+                // one each
+                int take = nums1[idx1] * nums2[idx2];
+                int next =  dp[idx1+1][idx2+1];
+                if (next != INT_MIN) take = max(take, take+next);
+                // skip nums1
+                int skip1 = dp[idx1+1][idx2];
+                // skip nums2
+                int skip2 = dp[idx1][idx2+1];
+
+                dp[idx1][idx2] = max({take, skip1, skip2});
+            }
+        }
+        return dp[0][0];
     }
 };
