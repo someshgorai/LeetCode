2126751e01aa1445e3b577b74fb23667ce4c25b3
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-01-08 22:06:26 +0530

    Time: 35 ms (43.20%) | Memory: 19.1 MB (18.80%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,25 +1,26 @@
 class Solution {
 private:
-    int backtrack(int idx1, int idx2, vector<int>& nums1, vector<int>& nums2, int s1, int s2, bool taken, vector<vector<int>> &dp) {
+    int backtrack(int idx1, int idx2, vector<int>& nums1, vector<int>& nums2, int s1, int s2, vector<vector<int>> &dp) {
         // Base Case
-        if (idx1 == s1 || idx2 == s2) {
-            if (taken) return 0;
-            else return INT_MIN;
-        }
+        if (idx1 == s1 || idx2 == s2) return INT_MIN;
 
         if (dp[idx1][idx2] != INT_MIN) return dp[idx1][idx2];
 
-        int take = INT_MIN;
-        for (int i=idx2; i<s2; i++) {
-            take = max(nums1[idx1] * nums2[i] + backtrack(idx1+1, i+1, nums1, nums2, s1, s2, true, dp), take);
-        }
-        
-        return dp[idx1][idx2] = max(take, backtrack(idx1+1, idx2, nums1, nums2, s1, s2, taken, dp));
+        // one each
+        int take = nums1[idx1] * nums2[idx2];
+        int next =  backtrack(idx1+1, idx2+1, nums1, nums2, s1, s2, dp);
+        if (next != INT_MIN) take = max(take, take+next);
+        // skip nums1
+        int skip1 = backtrack(idx1+1, idx2, nums1, nums2, s1, s2, dp);
+        // skip nums2
+        int skip2 = backtrack(idx1, idx2+1, nums1, nums2, s1, s2, dp);
+
+        return dp[idx1][idx2] = max({take, skip1, skip2});
     }
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int s1 = nums1.size(), s2 = nums2.size();
         vector<vector<int>> dp(s1, vector<int> (s2, INT_MIN));
-        return backtrack(0, 0, nums1, nums2, nums1.size(), nums2.size(), false, dp);
+        return backtrack(0, 0, nums1, nums2, nums1.size(), nums2.size(), dp);
     }
 };
