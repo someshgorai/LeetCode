c1340613bd8fdee05fe54a16ed8b58a0ed356104
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-01-09 10:44:23 +0530

    Time: 19 ms (56.53%) | Memory: 34.5 MB (95.43%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
+    vector<int> DIR = {0, 1, 0, -1, 0};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        if (mat.empty() || mat[0].empty())
-            return {};
-
         int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> queue;
-        int MAX_VALUE = m * n;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    queue.push({i, j});
-                } else {
-                    mat[i][j] = MAX_VALUE;
-                }
+        queue<pair<int, int>> q;
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (mat[r][c] == 0) q.emplace(r, c);
+                else mat[r][c] = -1; // Marked as not processed yet!
+
+        while (!q.empty()) {
+            auto [r, c] = q.front(); q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + DIR[i], nc = c + DIR[i+1];
+                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
+                mat[nr][nc] = mat[r][c] + 1;
+                q.emplace(nr, nc);
             }
         }
-        
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        while (!queue.empty()) {
-            auto [row, col] = queue.front(); queue.pop();
-            for (auto [dr, dc] : directions) {
-                int r = row + dr, c = col + dc;
-                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[row][col] + 1) {
-                    queue.push({r, c});
-                    mat[r][c] = mat[row][col] + 1;
-                }
-            }
-        }
-        
         return mat;
     }
 };
