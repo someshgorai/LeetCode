075ad8a2e7c8c4dc91470d8eac9df95247e8197c
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-01-10 12:33:24 +0530

    Time: 31 ms (95.13%) | Memory: 19.2 MB (95.90%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string, int>> q;
-        set<string> st(wordList.begin(), wordList.end());
-        st.erase(beginWord);
-        q.emplace(beginWord, 1);
-        while (!q.empty()) {
-            string word = q.front().first;
-            int step = q.front().second;
-            q.pop();
-            
-            for (int i=0; i<word.size(); i++) {
-                char original = word[i];
-                for (char ch='a'; ch<='z'; ch++) {
-                    word[i] = ch;
-            
-                    if (st.find(word) != st.end()) {
-                        st.erase(word);
-                        q.emplace(word, step+1);
-                        if (word == endWord) return step+1;
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        if (!dict.count(endWord)) return 0;
+
+        unordered_set<string> beginSet{beginWord}, endSet{endWord};
+        int level = 1;
+        int L = beginWord.size();
+
+        while (!beginSet.empty() && !endSet.empty()) {
+            // Always expand the smaller set
+            if (beginSet.size() > endSet.size())
+                swap(beginSet, endSet);
+
+            unordered_set<string> nextLevel;
+
+            for (string word : beginSet) {
+                for (int i = 0; i < L; i++) {
+                    char original = word[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        word[i] = c;
+                        if (endSet.count(word)) return level + 1;
+                        if (dict.count(word)) {
+                            nextLevel.insert(word);
+                            dict.erase(word);
+                        }
                     }
+                    word[i] = original;
                 }
-                word[i] = original;
             }
+
+            beginSet = move(nextLevel);
+            level++;
         }
         return 0;
     }
