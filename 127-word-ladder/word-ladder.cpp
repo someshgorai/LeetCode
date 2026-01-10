e774cc7f51fcfe93691a485db0d294fd0942cb47
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-01-10 12:28:47 +0530

    Time: 330 ms (26.97%) | Memory: 21.2 MB (58.75%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string, int>> q;
+        set<string> st(wordList.begin(), wordList.end());
+        st.erase(beginWord);
+        q.emplace(beginWord, 1);
+        while (!q.empty()) {
+            string word = q.front().first;
+            int step = q.front().second;
+            q.pop();
+            
+            for (int i=0; i<word.size(); i++) {
+                char original = word[i];
+                for (char ch='a'; ch<='z'; ch++) {
+                    word[i] = ch;
+            
+                    if (st.find(word) != st.end()) {
+                        st.erase(word);
+                        q.emplace(word, step+1);
+                        if (word == endWord) return step+1;
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return 0;
+    }
+};
