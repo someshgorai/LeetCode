6a90b0a91fe05801bb06b91c82e6609082a3be51
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-01-29 14:34:19 +0530

    Time: 23 ms (41.26%) | Memory: 23.7 MB (75.94%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        vector<vector<int>> dist(n, vector<int> (n, 1e9));
+        dist[0][0] = 1;
+        priority_queue<pair<int, pair<int, int>>, 
+            vector<pair<int, pair<int, int>>>, 
+            greater<pair<int, pair<int, int>>>> pq;
+
+        pq.push({1, {0, 0}});
+        int drow[8] = {-1, -1, 0, +1, +1, +1, 0, -1};
+        int dcol[8] = {0, +1, +1, +1, 0, -1, -1, -1};
+        while (!pq.empty()) {
+            int travelled = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            pq.pop();
+            if (travelled > dist[x][y]) continue;
+            for (int i = 0; i < 8; i++) {
+                int r = x + drow[i];
+                int c = y + dcol[i];
+                if (r < n && c < n && r >= 0 && c >= 0 && grid[r][c] == 0) {
+                    if (travelled + 1 < dist[r][c]) {
+                        pq.push({travelled + 1, {r, c}});
+                        dist[r][c] = travelled + 1;
+                    }
+                }
+            }
+        }
+        if (dist[n-1][n-1] >= 1e9) return -1;
+        return dist[n-1][n-1];
+    }
+};
