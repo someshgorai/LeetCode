0cbede1ab0629aa0064ec7dbe468d8b60c140dd5
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-02-16 16:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.07%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    int reverseBits(int n) {
-        long long bitset = 0;
-        int temp = n;
-        int res = 0;
-        for (int i = 0; i < 32; i++) {
-            res <<= 1;
-            res = res | (temp & 1);
-            temp >>= 1;
+    uint32_t reverseBits(uint32_t n) {
+        static uint8_t table[256] = {0};
+        static bool initialized = false;
+
+        if (!initialized) {
+            for (int i = 0; i < 256; i++) {
+                uint8_t x = i, rev = 0;
+                for (int j = 0; j < 8; j++) {
+                    rev = (rev << 1) | (x & 1);
+                    x >>= 1;
+                }
+                table[i] = rev;
+            }
+            initialized = true;
         }
-        return res;
+
+        return (table[n & 0xff] << 24) |
+               (table[(n >> 8) & 0xff] << 16) |
+               (table[(n >> 16) & 0xff] << 8) |
+               (table[(n >> 24) & 0xff]);
     }
 };
