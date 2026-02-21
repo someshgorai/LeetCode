3b81ba518b4b863679966f6c9b63fd94c88a0c4d
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-02-22 00:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (13.87%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,21 +1,10 @@
 class Solution {
-private:
-    bool isPrime(int n) {
-        if (n <= 1) return false;
-        if (n == 2 || n == 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false;
-
-        for (int i = 5; i*i < n; i = i + 6) {
-            if (n % i == 0 || n % (i+2) == 0) return false;
-        }
-
-        return true;
-    }
 public:
     int countPrimeSetBits(int left, int right) {
         int count = 0;
+        unordered_set<int> primes = { 2, 3, 5, 7, 11, 13, 17, 19};
         for (int i = left; i <= right; i++) {
-            if (isPrime(__builtin_popcount(i))) {
+            if (primes.count(__builtin_popcount(i))) {
                 count++;
             }
         }
