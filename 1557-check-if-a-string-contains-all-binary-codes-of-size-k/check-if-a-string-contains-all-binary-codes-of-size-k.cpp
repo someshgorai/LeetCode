7f9bfa76919dacfaa027b7f67a1dd7539fc04477
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-02-24 00:42:02 +0530

    Time: 535 ms (29.89%) | Memory: 150.3 MB (30.07%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -7,8 +7,12 @@ public:
         int m = 1 << k;
         unordered_set<string> vis;
 
+        int count = 0;
         for (int i = 0; i <= n - k; i++) {
-            vis.insert(s.substr(i, k));
+            string code = s.substr(i, k);
+            if (vis.count(code) == 0) count++;
+            if (count == m) return true;
+            vis.insert(code);
         }
         return vis.size() == m;
     }
