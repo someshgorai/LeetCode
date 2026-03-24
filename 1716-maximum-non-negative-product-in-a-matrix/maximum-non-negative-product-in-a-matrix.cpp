508c45a6fbf12370a0169e7a1852dfcc930a2e63
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-03-24 11:55:39 +0530

    Time: 3 ms (34.08%) | Memory: 14 MB (87.67%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -1,51 +1,51 @@
 class Solution {
-private:
-    vector<vector<pair<long long, long long>>> dp;
-
-    pair<long long, long long> solve(int i, int j, int m, int n, vector<vector<int>>& grid) {
-        // Base case
-        if (i == m-1 && j == n-1) {
-            return {grid[i][j], grid[i][j]};
-        }
-
-        if (dp[i][j].first != LLONG_MIN) return dp[i][j];
-
-        long long maxi = LLONG_MIN;
-        long long mini = LLONG_MAX;
-
-        // Move Down
-        if (i + 1 < m) {
-            auto down = solve(i+1, j, m, n, grid);
-
-            long long a = down.first * grid[i][j];
-            long long b = down.second * grid[i][j];
-
-            maxi = max({maxi, a, b});
-            mini = min({mini, a, b});
-        }
-
-        // Move Right
-        if (j + 1 < n) {
-            auto right = solve(i, j+1, m, n, grid);
-
-            long long a = right.first * grid[i][j];
-            long long b = right.second * grid[i][j];
-
-            maxi = max({maxi, a, b});
-            mini = min({mini, a, b});
-        }
-
-        return dp[i][j] = {maxi, mini};
-    }
-
 public:
     int maxProductPath(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        dp.assign(m, vector<pair<long long, long long>>(n, {LLONG_MIN, LLONG_MIN}));
+        int MOD = 1e9 + 7;
 
-        auto res = solve(0, 0, m, n, grid);
+        vector<vector<pair<long long,long long>>> dp(m, vector<pair<long long,long long>>(n));
 
-        if (res.first < 0) return -1;
-        return res.first % 1000000007;
+        // Base case
+        dp[m-1][n-1] = {grid[m-1][n-1], grid[m-1][n-1]};
+
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+
+                if (i == m-1 && j == n-1) continue;
+
+                long long maxi = LLONG_MIN;
+                long long mini = LLONG_MAX;
+
+                // Down
+                if (i + 1 < m) {
+                    auto down = dp[i+1][j];
+
+                    long long a = grid[i][j] * down.first;
+                    long long b = grid[i][j] * down.second;
+
+                    maxi = max({maxi, a, b});
+                    mini = min({mini, a, b});
+                }
+
+                // Right
+                if (j + 1 < n) {
+                    auto right = dp[i][j+1];
+
+                    long long a = grid[i][j] * right.first;
+                    long long b = grid[i][j] * right.second;
+
+                    maxi = max({maxi, a, b});
+                    mini = min({mini, a, b});
+                }
+
+                dp[i][j] = {maxi, mini};
+            }
+        }
+
+        long long res = dp[0][0].first;
+
+        if (res < 0) return -1;
+        return res % MOD;
     }
 };
