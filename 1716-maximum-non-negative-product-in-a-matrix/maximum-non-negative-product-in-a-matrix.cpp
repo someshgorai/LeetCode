84f1f54de0816c6c26e727549bdd9677c8ae0059
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-03-24 11:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (93.50%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -4,22 +4,23 @@ public:
         int m = grid.size(), n = grid[0].size();
         int MOD = 1e9 + 7;
 
-        vector<vector<pair<long long,long long>>> dp(m, vector<pair<long long,long long>>(n));
-
-        // Base case
-        dp[m-1][n-1] = {grid[m-1][n-1], grid[m-1][n-1]};
+        vector<pair<long long,long long>> dp(n);
 
         for (int i = m-1; i >= 0; i--) {
             for (int j = n-1; j >= 0; j--) {
 
-                if (i == m-1 && j == n-1) continue;
+                // Base case
+                if (i == m-1 && j == n-1) {
+                    dp[j] = {grid[i][j], grid[i][j]};
+                    continue;
+                }
 
                 long long maxi = LLONG_MIN;
                 long long mini = LLONG_MAX;
 
-                // Down
+                // Down → dp[j] (old value)
                 if (i + 1 < m) {
-                    auto down = dp[i+1][j];
+                    auto down = dp[j];
 
                     long long a = grid[i][j] * down.first;
                     long long b = grid[i][j] * down.second;
@@ -28,9 +29,9 @@ public:
                     mini = min({mini, a, b});
                 }
 
-                // Right
+                // Right → dp[j+1] (already updated)
                 if (j + 1 < n) {
-                    auto right = dp[i][j+1];
+                    auto right = dp[j+1];
 
                     long long a = grid[i][j] * right.first;
                     long long b = grid[i][j] * right.second;
@@ -39,11 +40,11 @@ public:
                     mini = min({mini, a, b});
                 }
 
-                dp[i][j] = {maxi, mini};
+                dp[j] = {maxi, mini};
             }
         }
 
-        long long res = dp[0][0].first;
+        long long res = dp[0].first;
 
         if (res < 0) return -1;
         return res % MOD;
