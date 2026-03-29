4fede4eb72d32eade107df6a7e9ac4845029a0da
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-03-29 22:33:44 +0530

    Time: 1 ms (96.67%) | Memory: 19.8 MB (70.67%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        int even[26] = {0}, odd[26] = {0};
+        int n = s1.size();
+        for (int i = 0; i < n; i++) {
+            if (i & 1) {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+            else {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (even[i] != 0 || odd[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
