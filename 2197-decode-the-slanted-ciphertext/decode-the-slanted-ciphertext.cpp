d50f429393de94d0edff2c673ffce054652a271a
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-04-04 20:35:35 +0530

    Time: 39 ms (45.76%) | Memory: 46.2 MB (37.29%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        string decodedText = "";
+        int len  = encodedText.size();
+        int cols = len / rows;
+        for (int i = 0; i <= cols; i++) {
+            for (int j = i; j < len; j += cols+1) {
+                decodedText += encodedText[j];
+            }
+        }
+        while (!decodedText.empty() && decodedText.back() == ' ') {
+            decodedText.pop_back();
+        }
+        return decodedText;
+    }
+};
