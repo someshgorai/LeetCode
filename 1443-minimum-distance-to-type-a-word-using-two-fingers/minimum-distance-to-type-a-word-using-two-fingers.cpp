4bdba4e08866d5d3fb9598c462d3af2980ad9929
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-04-13 02:02:03 +0530

    Time: 7 ms (71.98%) | Memory: 9.6 MB (71.98%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,27 +1,34 @@
 class Solution {
 private:
-    int dp[301][7][7][7][7];
-    int solve(string &word, int n, int i, int x1, int y1, int x2, int y2) {
+    int dp[301][27][27];
+    pair<int, int> getCoordinate(int c) {
+        return {c % 6, c / 6};
+    }
+    int getDistance(int x1, int y1, int x2, int y2) {
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
+    int solve(string &word, int n, int i, int c1, int c2) {
         if (i >= n) return 0;
 
-        if (dp[i+1][x1+1][y1+1][x2+1][y2+1] != -1) return dp[i+1][x1+1][y1+1][x2+1][y2+1];
+        if (dp[i][c1][c2] != -1) return dp[i][c1][c2];
 
-        char c = word[i];
-        int x = (c - 'A') % 6;
-        int y = (c - 'A') / 6;
+        auto [x, y] = getCoordinate(word[i] - 'A');
 
-        int moveFinger1 = solve(word, n, i + 1, x, y, x2, y2);
-        int moveFinger2 = solve(word, n, i + 1, x1, y1, x, y);
+        int moveFinger1 = solve(word, n, i + 1, word[i] - 'A', c2);
+        int moveFinger2 = solve(word, n, i + 1, c1, word[i] - 'A');
 
-        if (x1 != -1 && y1 != -1) moveFinger1 += abs(x1 - x) + abs(y1 - y);
-        if (x2 != -1 && y2 != -1) moveFinger2 += abs(x2 - x) + abs(y2 - y);
+        auto [x1, y1] = getCoordinate(c1);
+        auto [x2, y2] = getCoordinate(c2);
 
-        return dp[i+1][x1+1][y1+1][x2+1][y2+1] = min(moveFinger1, moveFinger2);
+        if (c1 != 26) moveFinger1 += getDistance(x, y, x1, y1);
+        if (c2 != 26) moveFinger2 += getDistance(x, y, x2, y2);
+
+        return dp[i][c1][c2] = min(moveFinger1, moveFinger2);
     }
 public:
     int minimumDistance(string word) {
         int n = word.size();
         memset(dp, -1, sizeof(dp));
-        return solve(word, n, 0, -1, -1, -1, -1);
+        return solve(word, n, 0, 26, 26);
     }
 };
