c732f34aaacf902f7598dc0cae3ca1b0a4f9ea41
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-04-13 02:17:16 +0530

    Time: 43 ms (36.58%) | Memory: 8.8 MB (91.44%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int dp[301][27][27];
+    int dp[27][27];
 
     pair<int, int> getCoordinate(int c) {
         return {c % 6, c / 6};
@@ -8,25 +8,6 @@ private:
 
     int getDistance(int x1, int y1, int x2, int y2) {
         return abs(x1 - x2) + abs(y1 - y2);
-    }
-
-    int solve(string &word, int n, int i, int c1, int c2) {
-        if (i >= n) return 0;
-
-        if (dp[i][c1][c2] != -1) return dp[i][c1][c2];
-
-        auto [x, y] = getCoordinate(word[i] - 'A');
-
-        int moveFinger1 = solve(word, n, i + 1, word[i] - 'A', c2);
-        int moveFinger2 = solve(word, n, i + 1, c1, word[i] - 'A');
-
-        auto [x1, y1] = getCoordinate(c1);
-        auto [x2, y2] = getCoordinate(c2);
-
-        if (c1 != 26) moveFinger1 += getDistance(x, y, x1, y1);
-        if (c2 != 26) moveFinger2 += getDistance(x, y, x2, y2);
-
-        return dp[i][c1][c2] = min(moveFinger1, moveFinger2);
     }
 public:
     int minimumDistance(string word) {
@@ -38,8 +19,8 @@ public:
                 for (int c2 = 0; c2 <= 26; c2++) {
                     auto [x, y] = getCoordinate(word[i] - 'A');
 
-                    int moveFinger1 = dp[i + 1][word[i] - 'A'][c2];
-                    int moveFinger2 = dp[i + 1][c1][word[i] - 'A'];
+                    int moveFinger1 = dp[word[i] - 'A'][c2];
+                    int moveFinger2 = dp[c1][word[i] - 'A'];
 
                     auto [x1, y1] = getCoordinate(c1);
                     auto [x2, y2] = getCoordinate(c2);
@@ -47,11 +28,11 @@ public:
                     if (c1 != 26) moveFinger1 += getDistance(x, y, x1, y1);
                     if (c2 != 26) moveFinger2 += getDistance(x, y, x2, y2);
 
-                    dp[i][c1][c2] = min(moveFinger1, moveFinger2);
+                    dp[c1][c2] = min(moveFinger1, moveFinger2);
                 }
             }
         }
 
-        return dp[0][26][26];
+        return dp[26][26];
     }
 };
