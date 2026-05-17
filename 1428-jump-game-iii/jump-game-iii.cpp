bb7bb220f60d752313a1665e2eb690d02e82d94e
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-05-17 17:59:21 +0530

    Time: 3 ms (45.57%) | Memory: 35.6 MB (68.33%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,24 +1,24 @@
 class Solution {
-private:
-    bool backtrack(vector<int>& arr, vector<bool>& vis, int idx, int n) {
-        // Base Case
-        if (vis[idx]) return false;
-        if (arr[idx] == 0) return true;
-
-        vis[idx] = true;
-
-        bool res = false;
-
-        if (idx + arr[idx] < n) res = backtrack(arr, vis, idx + arr[idx], n);
-        if (idx - arr[idx] >= 0) res = res || backtrack(arr, vis, idx - arr[idx], n);
-
-        return res;
-    }
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        vector<bool> vis(n, false);
+        vector<bool> vis(n);
 
-        return backtrack(arr, vis, start, n);
+        queue<int> q;
+        q.push(start);
+
+        while (!q.empty()) {
+            int idx = q.front();
+            q.pop();
+
+            if (idx < 0 || idx >= n || vis[idx]) continue;
+
+            if (arr[idx] == 0) return true;
+            vis[idx] = true;
+            q.push(idx + arr[idx]);
+            q.push(idx - arr[idx]); 
+        }
+
+        return false;
     }
 };
