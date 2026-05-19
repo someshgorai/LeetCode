217ec5a993c5b5ab2be9e0c12a6067bd815bcf2b
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-05-19 17:44:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (88.83%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,23 +1,23 @@
 class Solution {
 private:
-    int get_nCr(int n, int r) {
-        long long res = 1;
-        for (int i = 0; i < r; i++) {
-            res = res * (n - i);
-            res = res / (long long)(i+1);
+    vector<int> generateRow(int row) {
+        vector<int> layer;
+        layer.push_back(1);
+        int cell = 1;
+        for (int col = 1; col < row; col++) {
+            cell = cell*(row-col);
+            cell = cell / col;
+            layer.push_back(cell);
         }
-        return (int)res;
+        return layer;
     }
 public:
     vector<vector<int>> generate(int numRows) {
         if (numRows == 1) return {{1}};
-        vector<vector<int>> pascal(numRows);
-        pascal[0].push_back(1);
+        vector<vector<int>> pascal;
 
-        for (int row = 1; row < numRows; row++) {
-            for (int col = 0; col <= row; col++) {
-                pascal[row].push_back(get_nCr(row, col));
-            }
+        for (int row = 1; row <= numRows; row++) {
+            pascal.push_back(generateRow(row));
         }
 
         return pascal;
