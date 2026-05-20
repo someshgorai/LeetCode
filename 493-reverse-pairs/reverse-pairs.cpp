32bba3091ef5e0540a474a1d0b2b9c364a15e451
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-05-20 12:11:03 +0530

    Time: 617 ms (5.13%) | Memory: 248.7 MB (15.27%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,65 @@
+class Solution {
+private:
+    int helper(vector<int>& nums, int low, int mid, int high) {
+        int cnt = 0;
+        int right = mid + 1;
+
+        for (int left = low; left <= mid; left++) {
+            while (right <= high && (long long)nums[left] > 2LL * nums[right]) {
+                right++;
+            }
+
+            cnt += (right - (mid + 1));
+        }
+
+        return cnt;
+    }
+    int merge(vector<int>& nums, int low, int mid, int high) {
+        int cnt = helper(nums, low, mid, high);
+        vector<int> temp;
+        int left = low;
+        int right = mid+1;
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else {
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.push_back(nums[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.push_back(nums[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+
+        return cnt;
+    }
+    int mergeSort(vector<int>& nums, int low, int high) {
+        int cnt = 0;
+        if (low == high) return cnt;
+        int mid = (high - low) / 2 + low;
+
+        cnt += mergeSort(nums, low, mid);
+        cnt += mergeSort(nums, mid+1, high);
+        cnt += merge(nums, low, mid, high);
+
+        return cnt;
+    }
+public:
+    int reversePairs(vector<int>& nums) {
+        vector<int> temp = nums;
+        return mergeSort(temp, 0, nums.size() - 1);
+    }
+};
