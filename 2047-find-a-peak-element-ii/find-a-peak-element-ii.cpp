9f4f6c5d1bbd702af96a20ed71d03868ace158e9
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-05-22 23:24:05 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (68.23%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,36 +1,33 @@
 class Solution {
+private:
+    int getMaxElement(vector<vector<int>>& mat, int col) {
+        int m = mat.size();
+        int maxi = INT_MIN;
+        int idx = 0;
+        for (int i = 0; i < m; i++) {
+            if (maxi < mat[i][col]) {
+                maxi = mat[i][col];
+                idx = i;
+            }
+        }
+        return idx;
+    }
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        int n = mat[0].size(), m = mat.size();
+        int low = 0, high = n - 1;
 
-        pq.push({mat[0][0], {0 , 0}});
-        vis[0][0] = true;
+        while (low <= high) {
+            int mid = (high - low)/2 + low;
+            int maxIdx = getMaxElement(mat, mid);
 
-        int drow[] = {1, 0, -1, 0};
-        int dcol[] = {0, 1, 0, -1};
+            int val = mat[maxIdx][mid];
+            int left = mid-1 >= 0 ? mat[maxIdx][mid-1] : -1;
+            int right = mid + 1 < n? mat[maxIdx][mid+1] : -1;
 
-        while (!pq.empty()) {
-            auto [val, pos] = pq.top();
-            auto [i, j] = pos;
-            vis[i][j] = true;
-            pq.pop();
-            bool greater = true;
-
-            for (int k = 0; k < 4; k++) {
-                int row = i + drow[k];
-                int col = j + dcol[k];
-
-                if (row >= 0 && row < m && col >= 0 && col < n) {
-                    if (mat[row][col] > val) greater = false;
-                    if (!vis[row][col]) {
-                        vis[row][col] = true;
-                        pq.push({mat[row][col], {row, col}});
-                    }
-                }
-            }
-            if (greater) return {i, j};
+            if (val > left && val > right) return {maxIdx, mid};
+            else if(val < left) high = mid - 1;
+            else low = mid + 1;
         }
 
         return {-1, -1};
