495ad1289122d6c908762d93e9058a0c30a455a6
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-05-25 10:09:11 +0530

    Time: 35 ms (18.37%) | Memory: 24 MB (13.70%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    int getJumps(int idx, vector<int>& nums, int d, int n, vector<int>& dp) {
+        if (dp[idx] != -1) return dp[idx];
+
+        int maxJumps = 1;
+        for (int x = 1; x <= d; x++) {
+            if (idx + x < n && nums[idx] > nums[idx + x]) {
+                maxJumps = max(maxJumps, 1+getJumps(idx+x, nums, d, n, dp));
+            }
+            else break;
+        }
+
+        for (int x = 1; x <= d; x++) {
+            if (idx - x >= 0 && nums[idx] > nums[idx - x]) {
+                maxJumps = max(maxJumps, 1+getJumps(idx-x, nums, d, n, dp));
+            }
+            else break;
+        }
+
+        return dp[idx] = maxJumps;
+    }
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        set<pair<int, int>> hash;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            hash.insert({arr[i], i});
+        }
+
+        vector<int> dp(n, -1);
+        int maxTowers = 0;
+        for (auto it : hash) {
+            auto [height, start] = it;
+            maxTowers = max(maxTowers, getJumps(start, arr, d, n, dp));
+        }
+
+        return maxTowers;
+    }
+};
