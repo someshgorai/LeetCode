ad527c4cf053502601e7f4a78e078d7b2119db00
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-05-26 11:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (92.01%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,35 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy;
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                dummy->next = list1;
-                dummy = list1;
-                list1 = list1->next;
-            }
-            else {
-                dummy->next = list2;
-                dummy = list2;
-                list2 = list2->next;
-            }
-        }
-        if (list1 != nullptr) dummy->next = list1;
-        else dummy->next = list2;
 
-        return temp->next;
-    }
+class Solution {
+private:
+    struct compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = lists.size();
-        ListNode* current = nullptr;
-        for (int i = 0; i < n; i++) {
-            current = mergeTwoLists(current, lists[i]);
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        for (ListNode* node : lists) {
+            if (node) pq.push(node);
         }
-        return current;
+        
+        ListNode* prehead = new ListNode();
+        ListNode* current = prehead;
+        while(!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+            current->next = node;
+            current = node;
+            if(node->next) pq.push(node->next);
+        }
+        return prehead->next;
     }
 };
