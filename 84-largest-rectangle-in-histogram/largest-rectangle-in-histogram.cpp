c4d6b8890824b58fbe526b961c8f68b72988a24d
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-01 20:07:07 +0530

    Time: 28 ms (32.51%) | Memory: 85.9 MB (22.56%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,23 +1,41 @@
 class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
+private:
+    vector<int> getNSE(vector<int> &heights) {
         int n = heights.size();
+        vector<int> res(n, n);
         stack<int> st;
-        vector<int> pse(n, -1);
-        int maxArea = 0;
-        for (int i=0; i<n; i++) {
-            
-            while (!st.empty() && heights[st.top()] > heights[i]) {
-                int idx = st.top();
-                maxArea = max(maxArea, heights[idx] * (i - pse[idx] - 1));               
+        for (int i = n-1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
                 st.pop();
             }
-            if (!st.empty()) pse[i] = st.top();
+            if (!st.empty()) 
+            res[i] = st.top();;
             st.push(i);
         }
-        while (!st.empty()) {
-            maxArea = max(maxArea, heights[st.top()] * (n - pse[st.top()] - 1));
-            st.pop();
+        return res;
+    }
+    vector<int> getPSE(vector<int> &heights) {
+        int n = heights.size();
+        vector<int> res(n, -1);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if (!st.empty()) res[i] = st.top();;
+            st.push(i);
+        }
+        return res;
+    }
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> nse = getNSE(heights);
+        vector<int> pse = getPSE(heights);
+        int n = heights.size();
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            maxArea = max(maxArea, heights[i] * (nse[i] - pse[i] - 1));
         }
 
         return maxArea;
