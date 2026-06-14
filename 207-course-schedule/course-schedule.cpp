de93737ec4670fff2f22b51d57dfdf26e281b399
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-14 21:51:01 +0530

    Time: 15 ms (9.81%) | Memory: 19.2 MB (80.87%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,45 @@
 class Solution {
-public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjlist(numCourses);
-        vector<int> indegree(numCourses, 0);
-        for (vector<int> it:prerequisites) {
-            adjlist[it[0]].push_back(it[1]);
-            indegree[it[1]]++;
-        }
-        queue<int> q;
-        for (int i=0; i<numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.emplace(i);
+private:
+    bool isCyclic(int node,
+                  vector<vector<int>>& graph,
+                  vector<bool>& vis,
+                  vector<bool>& pathVis) {
+
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for (int neigh : graph[node]) {
+            if (!vis[neigh]) {
+                if (isCyclic(neigh, graph, vis, pathVis))
+                    return true;
+            }
+            else if (pathVis[neigh]) {
+                return true; // back edge found
             }
         }
 
-        int cnt = 0;
-        while (!q.empty()) {
-            int course = q.front();
-            cnt++;
-            q.pop();
-            for (int node : adjlist[course]) {
-                indegree[node]--;
-                if (indegree[node] == 0){
-                    q.emplace(node);
-                }
+        pathVis[node] = false;
+        return false;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+
+        for (auto &pre : prerequisites) {
+            graph[pre[1]].push_back(pre[0]);
+        }
+
+        vector<bool> vis(numCourses, false);
+        vector<bool> pathVis(numCourses, false);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!vis[i]) {
+                if (isCyclic(i, graph, vis, pathVis))
+                    return false;
             }
         }
-        if (cnt == numCourses) return true;
-        return false;
+
+        return true;
     }
 };
