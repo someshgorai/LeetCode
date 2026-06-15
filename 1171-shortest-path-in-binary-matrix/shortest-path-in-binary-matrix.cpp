3e345a44289a23ab9ff9e990e7e966f3a1e1b3da
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-15 22:00:22 +0530

    Time: 51 ms (16.40%) | Memory: 36.9 MB (12.95%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if (grid[0][0] == 1) return -1;
         int n = grid.size();
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-        vector<vector<int>> dist(n, vector<int> (n, 1e9));
+        vector<vector<int>> dist(n, vector<int>(n, 1e9));
+        set<pair<int, pair<int, int>>> st;
         dist[0][0] = 1;
-        queue<pair<int, pair<int, int>>> q;
+        st.insert({1, {0, 0}}); 
 
-        q.push({1, {0, 0}});
-        int drow[8] = {-1, -1, 0, +1, +1, +1, 0, -1};
-        int dcol[8] = {0, +1, +1, +1, 0, -1, -1, -1};
-        while (!q.empty()) {
-            int travelled = q.front().first;
-            int x = q.front().second.first;
-            int y = q.front().second.second;
-            q.pop();
-            if (travelled > dist[x][y]) continue;
+        int drow[] = {1, 1, 0, -1, -1, -1, 0, 1};
+        int dcol[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+        while (!st.empty()) {
+            auto it = *st.begin();
+            st.erase(it);
+            auto [x, y] = it.second;
+            int travel = it.first;
+
             for (int i = 0; i < 8; i++) {
                 int r = x + drow[i];
                 int c = y + dcol[i];
-                if (r < n && c < n && r >= 0 && c >= 0 && grid[r][c] == 0) {
-                    if (travelled + 1 < dist[r][c]) {
-                        q.push({travelled + 1, {r, c}});
-                        dist[r][c] = travelled + 1;
+
+                if (r >= 0 && c >= 0 && r < n && c < n && grid[r][c] == 0) {
+                    if (travel + 1 < dist[r][c]) {
+                        if (dist[r][c] != 1e9) {
+                            st.erase({dist[r][c], {r, c}});   
+                        }
+                        st.insert({travel + 1, {r, c}});
+                        dist[r][c] = travel + 1;
+                        if (r == n-1 && c == n-1) return dist[r][c];
                     }
                 }
             }
         }
-        if (dist[n-1][n-1] >= 1e9) return -1;
-        return dist[n-1][n-1];
+
+        return dist[n-1][n-1] == 1e9 ? -1 : dist[n-1][n-1];
     }
 };
