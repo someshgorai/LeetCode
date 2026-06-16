b3c9f59251c51a0e8e703cebbf24499242bd8ad5
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-17 02:10:35 +0530

    Time: 91 ms (28.17%) | Memory: 79.8 MB (17.96%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
+    int findUltiCourse(int node, vector<int> &ultiCourse) {
+        if (ultiCourse[node] == node) return node;
+        return ultiCourse[node] = findUltiCourse(ultiCourse[node], ultiCourse);
+    }
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(numCourses, vector<int> (numCourses, 1e9));
-        for (int i = 0; i < numCourses; i++) {
-            adj[i][i] = 0;
-        }
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
 
         for (auto pre : prerequisites) {
-            adj[pre[0]][pre[1]] = 1; 
+            adj[pre[0]].push_back(pre[1]);
+            indegree[pre[1]]++;
         }
-
-        for (int via = 0; via < numCourses; via++) {
-            for (int i = 0; i < numCourses; i++) {
-                for (int j = 0; j < numCourses; j++) {
-                    adj[i][j] = min(adj[i][j], adj[i][via] + adj[via][j]);
-                }
+        
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
 
+        vector<unordered_set<int>> preCourse(numCourses);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int neighbor : adj[node]) {
+                preCourse[neighbor].insert(node);
+                for (auto pre : preCourse[node]) {
+                    preCourse[neighbor].insert(pre);
+                } 
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+        
         vector<bool> ans;
+        
         for (auto query : queries) {
-            if (adj[query[0]][query[1]] == 1e9) ans.push_back(false);
-            else ans.push_back(true);
+            int course1 = query[0];
+            int course2 = query[1];
+            if (preCourse[course2].empty()) ans.push_back(false);
+            else if (preCourse[course2].find(course1) != preCourse[course2].end()) ans.push_back(true);
+            else ans.push_back(false);
         }
 
         return ans;
