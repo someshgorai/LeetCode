872aa3f316617d490df92ce16cf04cfca8999bb0
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-19 23:47:54 +0530

    Time: 31 ms (90.74%) | Memory: 105.8 MB (62.90%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,36 +1,24 @@
 class Solution {
-private:
-    int getLastOpsIdx(vector<int> &nums, vector<long long> &prefix, int i, int k) {
-        if (i == 0) return i;
-
-        int low = 0, high = i - 1;
-
-        while (low <= high) {
-            int mid = (high - low)/2 + low;
-
-            long long costOps = (1LL)*(i - mid + 1)*nums[i] - (prefix[i] - prefix[mid] + nums[mid]);
-
-            if (costOps <= k) high = mid - 1;
-            else low = mid + 1;
-        }
-
-        return low;
-    }
 public:
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> prefix(n, 0);
-
         sort(nums.begin(), nums.end());
-        prefix[0] = nums[0];
 
+        int l = 0;
         int maxFreq = 1;
-        for (int i = 0; i < n; i++) {
-            if (i > 0) prefix[i] = (1LL)*nums[i] + prefix[i-1];
+        long long sum = 0;
 
-            int lastOpsIdx = getLastOpsIdx(nums, prefix, i, k);
+        for (int r = 0; r < n; r++) {
+            sum += nums[r];
+            long long opsCost = (1LL) * (r - l + 1) * nums[r] - sum;
 
-            maxFreq = max(maxFreq, i - lastOpsIdx + 1);
+            while (opsCost > k) {
+                opsCost = opsCost - (nums[r] - nums[l]);
+                sum = sum - nums[l];
+                l++;
+            }
+
+            maxFreq = max(maxFreq, (r - l + 1));
         }
 
         return maxFreq;
