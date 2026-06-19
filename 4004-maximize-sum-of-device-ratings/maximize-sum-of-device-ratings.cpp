95938ceba69cfa5fc619d6a5089a64c4fc1e2e43
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-19 10:50:06 +0530

    Time: 2 ms (90.98%) | Memory: 156.9 MB (46.99%) - LeetSync

--- 4004-maximize-sum-of-device-ratings/maximize-sum-of-device-ratings.cpp
+++ 4004-maximize-sum-of-device-ratings/maximize-sum-of-device-ratings.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
+int secondMax(vector<int>& nums, int& x){
+    if (nums.size() < 2) {
+        x = min(x, nums[0]);
+        return nums[0];  
+    }
+    int mini = INT_MAX;
+    int mini2 = INT_MAX;
+    int n = nums.size();
+    
+    for(int i=0; i<n; i++){
+        if(nums[i] < mini){
+            mini2 = mini;
+            mini = nums[i];
+            continue;
+        }
+        if(nums[i] < mini2){
+            mini2 = nums[i];
+        }
+    }
+    x = min(x, mini);
+    return mini2;
+}
     long long maxRatings(vector<vector<int>>& units) {
         int m = units.size();
-        int n = units[0].size();
-        long sum = 0;
-        if(n == 1) {
-            for(int i=0; i<m; i++) sum += units[i][0];
-            return sum;
+        long long ans = 0;
+        int x = INT_MAX;
+        int small = INT_MAX;
+        for(int i=0; i<m; i++){
+            int num = secondMax(units[i], x);
+            if(num < small){
+                small = num;
+            }
+            ans += num;
         }
-        int minFirst = INT_MAX;
-        long secondSum = 0;
-        for(int i=0; i<m; i++) {
-            sort(units[i].begin(), units[i].end());
-            minFirst = min(minFirst, units[i][0]);
-            secondSum += units[i][1];
-        }
-        for(int i=0; i<m; i++) {
-            long curr = secondSum - units[i][1] + minFirst;
-            sum = max(sum, curr);
-        }
-        return sum;
+        ans -= small;
+        ans += x;
+        return ans;
     }
 };
