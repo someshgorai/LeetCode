30ff85e03e84ea31b7607a9e4a7bb931750f25cd
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-21 10:44:23 +0530

    Time: 218 ms (45.16%) | Memory: 268.7 MB (65.77%) - LeetSync

---
+++ 4104-shortest-path-with-at-most-k-consecutive-identical-characters/shortest-path-with-at-most-k-consecutive-identical-characters.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int shortestPath(int n, vector<vector<int>>& edges, string labels, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto edge : edges) {
+            adj[edge[0]].push_back({edge[1], edge[2]});
+        }
+
+        vector<vector<int>> costs(n, vector<int> (k+1, 1e9));
+        using T = tuple<long long, int, int>; // {cost, node, cons}
+        priority_queue<T, vector<T>, greater<T>> pq;
+
+        costs[0][1] = 0;
+        pq.push({0, 0, 1});
+
+        while(!pq.empty()) {
+            auto [cost, node, cons] = pq.top();
+            pq.pop();
+
+            if (cost > costs[node][cons]) continue;
+            if (node == n-1) return (int)cost;
+
+            for (auto next : adj[node]) {
+                int newCons;
+                if (labels[node] == labels[next.first]) {
+                    newCons = cons + 1;
+                }
+                else {
+                    newCons = 1;
+                }
+
+                if (newCons > k) continue;
+
+                long long nd = cost + next.second;
+                if (nd < costs[next.first][newCons]) {
+                    costs[next.first][newCons] = nd;
+                    pq.push({nd, next.first, newCons});
+                }
+            }
+        }
+        return -1;
+    }
+};
