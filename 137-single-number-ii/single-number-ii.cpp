255388f0fe0bcb5e445f6073d57d22bb356d0c41
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-25 23:13:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (34.01%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-
-        if (n == 1) return nums[0];
-
-        for (int i = 1; i < n; i += 3) {
-            if (nums[i-1] != nums[i]) return nums[i-1];
+        int ones = 0, twos = 0;
+        
+        for (int i : nums) {
+            ones = (i ^ ones) & ~twos;
+            twos = (i ^ twos) & ~ones;
         }
-        return nums[n-1];
+        return ones;
     }
 };
