48a14a37c3899c290e8e059d405583aba233ef00
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-06-26 19:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (52.19%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,18 +1,9 @@
 class Solution {
-private:
-    int findOneBits(int num) {
-        int cnt = 0;
-        while (num) {
-            cnt += (num & 1);
-            num >>= 1;
-        }
-        return cnt;
-    }
 public:
     vector<int> sortByBits(vector<int>& arr) {
         auto comparator = [&](int &a, int &b) {
-            int a_bits = findOneBits(a);
-            int b_bits = findOneBits(b);
+            int a_bits = __builtin_popcount(a);
+            int b_bits = __builtin_popcount(b);
 
             if (a_bits == b_bits) return a < b;
             return a_bits < b_bits;
