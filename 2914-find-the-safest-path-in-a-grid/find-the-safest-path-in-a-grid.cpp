3e895ab8f9b946b64e68015c48bec17f333fec7d
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-01 23:07:55 +0530

    Time: 551 ms (33.56%) | Memory: 185.2 MB (40.54%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,20 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 private:
 
-    bool isPossible(vector<vector<int>>& dist,
-                    int sf,
-                    int n) {
+    bool isPossible(vector<vector<int>>& dist, int sf, int n) {
 
         if (dist[0][0] < sf)
             return false;
 
         queue<pair<int,int>> q;
-        vector<vector<bool>> vis(
-            n, vector<bool>(n, false)
-        );
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
 
         q.push({0,0});
         vis[0][0] = true;
@@ -35,11 +28,7 @@ private:
                 int nr = r + drow[k];
                 int nc = c + dcol[k];
 
-                if (nr >= 0 && nr < n &&
-                    nc >= 0 && nc < n &&
-                    !vis[nr][nc] &&
-                    dist[nr][nc] >= sf) {
-
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !vis[nr][nc] & dist[nr][nc] >= sf) {
                     vis[nr][nc] = true;
                     q.push({nr,nc});
                 }
@@ -55,9 +44,7 @@ public:
 
         int n = grid.size();
 
-        vector<vector<int>> dist(
-            n, vector<int>(n, INT_MAX)
-        );
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
 
         queue<pair<int,int>> q;
 
@@ -85,9 +72,7 @@ public:
                 int nr = r + drow[k];
                 int nc = c + dcol[k];
 
-                if (nr >= 0 && nr < n &&
-                    nc >= 0 && nc < n &&
-                    dist[nr][nc] == INT_MAX) {
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && dist[nr][nc] == INT_MAX) {
 
                     dist[nr][nc] =
                         dist[r][c] + 1;
