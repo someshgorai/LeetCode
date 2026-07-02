20d1e20659907e5543d72e17299d8061d9f20ce2
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-02 09:26:48 +0530

    Time: 10 ms (84.59%) | Memory: 32.3 MB (51.31%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -2,48 +2,38 @@ class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int n = grid[0].size(), m = grid.size();
-        queue<pair<int, pair<int, int>>> q;
+        queue<pair<int, int>> q;
 
-        vector<vector<vector<bool>>> vis(m, vector<vector<bool>>(n, vector<bool>(health+1, false)));
+        vector<vector<int>> cost(m, vector<int>(n, -1));
 
         if (grid[0][0] == 1) {
             if (health == 1) return false;
-            q.push({health - 1, {0, 0}});
-            vis[0][0][health - 1] = true;
+            q.push({0, 0});
+            cost[0][0] = 1;
         }
         else {
-            q.push({health, {0, 0}});
-            vis[0][0][health] = true;
+            q.push({0, 0});
+            cost[0][0] = 0;
         }
 
         int drow[4] = {1, 0, -1, 0};
         int dcol[4] = {0, 1, 0, -1};
 
         while (!q.empty()) {
-            auto [life, cell] = q.front();
-            auto [x, y] = cell;
+            auto [x, y] = q.front();
             q.pop();
 
-            if (x == m-1 && y == n-1) return true;
+            if (x == m-1 && y == n-1 && cost[x][y] < health) return true;
 
             for (int i = 0; i < 4; i++) {
                 int r = x + drow[i];
                 int c = y + dcol[i];
 
-                if (r >= 0 && r < m && c >= 0 && c < n && !vis[r][c][life] && grid[r][c] == 0) {
-                    q.push({life, {r, c}});
-                    vis[r][c][life] = true;
-                }
-            }
-
-            if (life > 1) {
-                for (int i = 0; i < 4; i++) {
-                    int r = x + drow[i];
-                    int c = y + dcol[i];
-
-                    if (r >= 0 && r < m && c >= 0 && c < n && !vis[r][c][life-1] && grid[r][c] == 1) {
-                        q.push({life - 1, {r, c}});
-                        vis[r][c][life - 1] = true;
+                if (r >= 0 && r < m && c >= 0 && c < n) {
+                    int h = cost[x][y] + grid[r][c];
+                    if (cost[r][c] == -1 || h < cost[r][c]) {
+                        q.push({r, c});
+                        cost[r][c] = h;
                     }
                 }
             }
