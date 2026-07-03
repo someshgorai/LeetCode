6574976932c9138e789b43a41e7ecb2fb52844fd
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-03 11:36:36 +0530

    Time: 12 ms (80.24%) | Memory: 32.7 MB (45.10%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int n = grid[0].size(), m = grid.size();
-        queue<pair<int, int>> q;
+        deque<pair<int, int>> dq;
 
         vector<vector<int>> cost(m, vector<int>(n, INT_MAX));
 
         if (grid[0][0] == 1) {
             if (health == 1) return false;
-            q.push({0, 0});
+            dq.push_back({0, 0});
             cost[0][0] = 1;
         }
         else {
-            q.push({0, 0});
+            dq.push_front({0, 0});
             cost[0][0] = 0;
         }
 
         int drow[4] = {1, 0, -1, 0};
         int dcol[4] = {0, 1, 0, -1};
 
-        while (!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
+        while (!dq.empty()) {
+            auto [x, y] = dq.front();
+            dq.pop_front();
 
             if (x == m-1 && y == n-1 && cost[x][y] < health) return true;
 
@@ -32,7 +32,8 @@ public:
                 if (r >= 0 && r < m && c >= 0 && c < n) {
                     int h = cost[x][y] + grid[r][c];
                     if (h < cost[r][c]) {
-                        q.push({r, c});
+                        if (cost[x][y]) dq.push_back({r, c});
+                        else dq.push_front({r, c});
                         cost[r][c] = h;
                     }
                 }
