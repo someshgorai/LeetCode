8712b96adb1d1d5ed90f6b5696443988c3a1d140
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-04 10:25:20 +0530

    Time: 516 ms (42.65%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,7 +1,7 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
 SELECT t.id 
 FROM Weather AS y
 CROSS JOIN
 Weather AS t
-WHERE (t.recordDate - y.recordDate) = 1 AND
+WHERE DATEDIFF(t.recordDate, y.recordDate) = 1 AND
 (t.temperature > y.temperature);
