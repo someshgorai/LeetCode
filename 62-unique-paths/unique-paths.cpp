2470092afb13721bf66319a46a9616764d0864e7
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-06 22:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (82.01%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,23 +1,25 @@
 class Solution {
-private:
-    int backtrack(int i, int j, vector<vector<int>> &dp) {
-        if (i == 0 && j == 0) return 1;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int paths = 0;
-        if (i-1 >= 0) {
-            paths += backtrack(i-1, j, dp);
-        }
-        if (j-1 >= 0) {
-            paths += backtrack(i, j-1, dp);
-        }
-
-        return dp[i][j] = paths;
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return backtrack(m-1, n-1, dp);
+        vector<int> dp(n, 0);
+
+        dp[0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                int paths = 0;
+                if (i-1 >= 0) {
+                    paths += dp[j];
+                }
+                if (j-1 >= 0) {
+                    paths += dp[j-1];
+                }
+
+                dp[j] = paths;
+            }
+        }
+
+        return dp[n-1];
     }
 };
