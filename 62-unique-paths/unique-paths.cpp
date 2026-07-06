08462dad7b4a21b4640fe74951d6a2a7268736c0
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-06 22:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (76.65%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -6,23 +6,18 @@ public:
         dp[0] = 1;
 
         for (int i = 0; i < m; i++) {
-            vector<int> temp(n, 0);
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    temp[0] = 1;
-                    continue;
-                }
+                if (i == 0 && j == 0) continue;
                 int paths = 0;
                 if (i-1 >= 0) {
                     paths += dp[j];
                 }
                 if (j-1 >= 0) {
-                    paths += temp[j-1];
+                    paths += dp[j-1];
                 }
 
-                temp[j] = paths;
+                dp[j] = paths;
             }
-            dp = temp;
         }
 
         return dp[n-1];
