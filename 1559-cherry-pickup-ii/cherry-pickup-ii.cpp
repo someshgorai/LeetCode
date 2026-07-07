cb41a4d6f0ba979fdf9f7eca3dc2b5a98c32d641
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-07 11:13:12 +0530

    Time: 45 ms (67.49%) | Memory: 21 MB (54.21%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
+        vector<vector<int>> dp (n, vector<int>(n, 0));
         int dir[3] = {-1, 0, 1};
 
         for (int c1 = 0; c1 < n; c1++) {
             for (int c2 = 0; c2 < n; c2++) {
-                if (c1 == c2) dp[m-1][c1][c2] = grid[m-1][c1];
-                else dp[m-1][c1][c2] = grid[m-1][c1] + grid[m-1][c2];
+                if (c1 == c2) dp[c1][c2] = grid[m-1][c1];
+                else dp[c1][c2] = grid[m-1][c1] + grid[m-1][c2];
             }
         }
 
         for (int r = m-2; r >= 0; r--) {
+            vector<vector<int>> temp(n, vector<int> (n, 0));
+
             for (int c1 = n-1; c1 >= 0; c1--) {
                 for (int c2 = n-1; c2 >= 0; c2--) {
                     int maxCherries = 0;
@@ -25,16 +27,17 @@ public:
 
                             if (y2 < 0 || y2 >= n) continue;
 
-                            maxCherries = max(maxCherries, dp[r+1][y1][y2]);
+                            maxCherries = max(maxCherries, dp[y1][y2]);
                         }
                     }
 
-                    if (c1 == c2) dp[r][c1][c2] = maxCherries + grid[r][c1];
-                    else dp[r][c1][c2] = maxCherries + grid[r][c1] + grid[r][c2];
+                    if (c1 == c2) temp[c1][c2] = maxCherries + grid[r][c1];
+                    else temp[c1][c2] = maxCherries + grid[r][c1] + grid[r][c2];
                 }
             }
+            dp = temp;
         }
 
-        return dp[0][0][n-1];
+        return dp[0][n-1];
     }
 };
