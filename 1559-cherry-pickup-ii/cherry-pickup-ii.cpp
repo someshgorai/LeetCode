e6f0564195021676dcbdd26e667e6f8f808e2296
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-07 11:01:19 +0530

    Time: 62 ms (30.74%) | Memory: 20.8 MB (74.37%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,38 +1,40 @@
 class Solution {
-private:
-    int dir[3] = {-1, 0, 1};
-    int backtrack(int r, int c1, int c2, int m, int n, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp) {
-        // Base Case
-        if (r == m-1) {
-            if (c1 == c2) return grid[r][c1];
-            else return grid[r][c1] + grid[r][c2];
-        }
-
-        if (dp[r][c1][c2] != -1) return dp[r][c1][c2];
-
-        int maxCherries = 0;
-        for (int k = 0; k < 3; k++) {
-            int y1 = c1 + dir[k];
-
-            if (y1 < 0 || y1 >= n) continue;
-            for (int l = 0; l < 3; l++) {
-                int y2 = c2 + dir[l];
-
-                if (y2 < 0 || y2 >= n) continue;
-
-                maxCherries = max(maxCherries, backtrack(r+1, y1, y2, m, n, grid, dp));
-            }
-        }
-
-        if (c1 == c2) {
-            return dp[r][c1][c2] = maxCherries + grid[r][c1];
-        }
-        return dp[r][c1][c2] = maxCherries + grid[r][c1] + grid[r][c2];
-    }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
-        return backtrack(0, 0, n-1, m, n, grid, dp);
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
+        int dir[3] = {-1, 0, 1};
+
+        for (int c1 = 0; c1 < n; c1++) {
+            for (int c2 = 0; c2 < n; c2++) {
+                if (c1 == c2) dp[m-1][c1][c2] = grid[m-1][c1];
+                else dp[m-1][c1][c2] = grid[m-1][c1] + grid[m-1][c2];
+            }
+        }
+
+        for (int r = m-2; r >= 0; r--) {
+            for (int c1 = n-1; c1 >= 0; c1--) {
+                for (int c2 = n-1; c2 >= 0; c2--) {
+                    int maxCherries = 0;
+                    for (int k = 0; k < 3; k++) {
+                        int y1 = c1 + dir[k];
+
+                        if (y1 < 0 || y1 >= n) continue;
+                        for (int l = 0; l < 3; l++) {
+                            int y2 = c2 + dir[l];
+
+                            if (y2 < 0 || y2 >= n) continue;
+
+                            maxCherries = max(maxCherries, dp[r+1][y1][y2]);
+                        }
+                    }
+
+                    if (c1 == c2) dp[r][c1][c2] = maxCherries + grid[r][c1];
+                    else dp[r][c1][c2] = maxCherries + grid[r][c1] + grid[r][c2];
+                }
+            }
+        }
+
+        return dp[0][0][n-1];
     }
 };
