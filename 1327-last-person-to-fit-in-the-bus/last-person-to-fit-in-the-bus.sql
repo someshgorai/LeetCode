48fda7205865c9cc5b59b2048f3c626eea82bd42
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-11 23:51:39 +0530

    Time: 772 ms (89.60%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,9 @@
 # Write your MySQL query statement below
 select person_name
-from queue as q
-join
-(select person_id,
+from 
+(select person_name,
 sum(weight) over(order by turn) as cumm
 from queue) as temp
-on q.person_id = temp.person_id
 where temp.cumm <= 1000 
 order by temp.cumm desc
 limit 1;
