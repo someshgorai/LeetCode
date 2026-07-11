2aed72448abea46b19c41699f21c126ce2c1d4ac
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-11 10:02:15 +0530

    Time: 536 ms (92.23%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,11 +1,18 @@
 # Write your MySQL query statement below
-SELECT ROUND(
-    COUNT(player_id) /
-    (SELECT COUNT(DISTINCT player_id) FROM activity),
-2) AS fraction
-FROM activity
-WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN
-    (SELECT player_id, MIN(event_date)
-    FROM activity
-    GROUP BY player_id);
-    
+SELECT
+  ROUND(
+    COUNT(A1.player_id)
+    / (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3)
+  , 2) AS fraction
+FROM
+  Activity A1
+WHERE
+  (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
+    SELECT
+      A2.player_id,
+      MIN(A2.event_date)
+    FROM
+      Activity A2
+    GROUP BY
+      A2.player_id
+  );
