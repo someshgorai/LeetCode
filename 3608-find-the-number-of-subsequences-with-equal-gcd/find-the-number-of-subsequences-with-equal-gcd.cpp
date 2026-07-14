15350f5c5cf36d7397a0590ab0354024270a0150
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-14 13:02:24 +0530

    Time: 1252 ms (27.82%) | Memory: 40.9 MB (86.09%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -4,10 +4,10 @@ public:
     int subsequencePairCount(vector<int>& nums) {
         int n = nums.size();
         int m = *max_element(nums.begin(), nums.end());
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(m+1, vector<int>(m+1, 0)));
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(m+1, vector<int>(m+1, 0)));
         
         for (int gcd = 1; gcd < m+1; gcd++) {
-            dp[n][gcd][gcd] = 1;
+            dp[1][gcd][gcd] = 1;
         }
 
         for (int idx = n - 1; idx >= 0; idx--) {
@@ -17,14 +17,15 @@ public:
                     int ng1 = (g1 == 0) ? nums[idx] : gcd(g1, nums[idx]);
                     int ng2 = (g2 == 0) ? nums[idx] : gcd(g2, nums[idx]);
 
-                    long long take1 = dp[idx + 1][ng1][g2];
-                    long long take2 = dp[idx + 1][g1][ng2];
-                    long long notTake = dp[idx + 1][g1][g2];
+                    long long take1 = dp[1][ng1][g2];
+                    long long take2 = dp[1][g1][ng2];
+                    long long notTake = dp[1][g1][g2];
 
-                    dp[idx][g1][g2] =
+                    dp[0][g1][g2] =
                         (take1 + take2 + notTake) % mod;
                 }
             }
+            dp[1] = dp[0];
         }
 
         return dp[0][0][0];
