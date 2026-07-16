e6925f0d6e8b0a2d388b8c98ba4600fa58fb6b24
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-16 19:29:48 +0530

    Time: 47 ms (34.74%) | Memory: 53.1 MB (8.94%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -5,19 +5,32 @@ class Solution {
             return 1e9;
         }
 
-        if (dp[idx][target] != -1) return dp[idx][target];
+        if (dp[idx+1][target] != -1) return dp[idx+1][target];
 
         int take = 1e9;
         if (target >= coins[idx]) take = backtrack(idx, target - coins[idx], coins, dp) + 1;
         int notTake = backtrack(idx-1, target, coins, dp);
 
-        return dp[idx][target] = min(take, notTake);
+        return dp[idx+1][target] = min(take, notTake);
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        int coin = backtrack(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n+1, vector<int> (amount+1, 1e9));
+
+        dp[0][0] = 0;
+
+        for(int idx = 0; idx < n; idx++) {
+            for (int target = 0; target <= amount; target++) {
+                int take = 1e9;
+                if (target >= coins[idx]) take = dp[idx+1][target - coins[idx]] + 1;
+                int notTake = dp[idx][target];
+
+                dp[idx+1][target] = min(take, notTake);
+            }
+        }
+
+        int coin = dp[n][amount];
         if (coin == 1e9) return -1;
         return coin;
     }
