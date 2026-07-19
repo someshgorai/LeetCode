42d45011cebb4fc6d0748cc64c73192f1614b28f
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-19 17:30:26 +0530

    Time: 31 ms (87.50%) | Memory: 9.8 MB (89.84%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,29 +1,34 @@
 class Solution {
-private:
-    bool LCS(int i, int j, string &s, string &p, vector<vector<int>> &dp) {
-        if (i < 0 && j < 0) return true;
-        if (i < 0) {
-            while (j >= 0) {
-                if (p[j] != '*') return false;
-                j--;
-            }
-            return true;
-        }
-        if (i >= 0 && j < 0) return false;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s[i] == p[j]) return dp[i][j] = LCS(i-1, j-1, s, p, dp);
-        else {
-            if (p[j] == '?') return dp[i][j] = LCS(i-1, j-1, s, p, dp);
-            if (p[j] == '*') return dp[i][j] = LCS(i-1, j-1, s, p, dp) || LCS(i, j-1, s, p, dp) || LCS(i-1, j, s, p, dp);
-        }
-        return dp[i][j] = false;
-    }
 public:
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return LCS(n-1, m-1, s, p, dp);
+        vector<bool> dp(m+1, false);
+
+        dp[0] = true;
+        for (int j = 1; j <= m; j++) {
+            if (p[j-1] == '*')
+                dp[j] = dp[j-1];
+            else
+                dp[j] = false;
+        }
+
+        bool dia, temp;
+
+        for (int i = 1; i <= n; i++) {
+            dia = dp[0];
+            dp[0] = false;
+            for (int j = 1; j <= m; j++) {
+                temp = dp[j];
+                if (s[i-1] == p[j-1]) dp[j] = dia;
+                else {
+                    if (p[j-1] == '?') dp[j] = dia;
+                    else if (p[j-1] == '*') dp[j] = dia || dp[j-1] || dp[j];
+                    else dp[j] = false;
+                }
+                dia = temp;
+            }
+        }
+
+        return dp[m];
     }
 };
