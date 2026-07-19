96555019a7b3fe53f1c6ffdbd2168fc55c307f22
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-07-19 21:37:18 +0530

    Time: 7 ms (40.08%) | Memory: 24.8 MB (23.42%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,23 +5,10 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(2, 0));
 
         for (int i = n-1; i >= 0; i--) {
-            for (int action = 0; action < 2; action++) {
-                int profit = 0;
-                if (action == 0) {
-                    profit = max(
-                        dp[i+1][1] - prices[i],
-                        dp[i+1][action]
-                    );
-                }
-                else {
-                    profit = max(
-                        prices[i] + dp[i+1][0],
-                        dp[i+1][action]
-                    );
-                }
 
-                dp[i][action] = profit;                
-            }
+            dp[i][0] = max(dp[i+1][1] - prices[i], dp[i+1][0]);
+            dp[i][1] = max(prices[i] + dp[i+1][0], dp[i+1][1]);
+
         }
 
         return dp[0][0];
