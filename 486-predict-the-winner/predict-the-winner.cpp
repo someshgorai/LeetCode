cb41bbd1348f1cc871d48b2a5a57aacc42d05c6c
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-02 16:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (16.32%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,30 +1,29 @@
 class Solution {
-private:
-    long long backtrack(vector<int> &nums, int left, int right, vector<vector<int>> &dp) {
-        if (left > right) {
-            return 0;
-        }
-        if (dp[left][right] != -1) return dp[left][right];
-
-        long long take_left = nums[left] + min(
-            backtrack(nums, left+2, right, dp),
-            backtrack(nums, left+1, right-1, dp)
-        );
-
-        long long take_right = nums[right] + min(
-            backtrack(nums, left+1, right-1, dp),
-            backtrack(nums, left, right-2, dp)
-        );
-
-        return dp[left][right] = max(take_left, take_right);
-    }
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
+        vector<vector<int>> dp(n+4, vector<int> (n+4, 0));
+
         long long total = accumulate(nums.begin(), nums.end(), 0LL);
 
-        long long score1 = backtrack(nums, 0, n-1, dp);
+        for (int left = n-1; left >= 0; left--) {
+            for (int right = 0; right < n; right++) {
+                if (left > right) continue;
+                long long take_left = nums[left] + min(
+                    dp[left+4][right+2],
+                    dp[left+3][right+1]
+                );
+
+                long long take_right = nums[right] + min(
+                    dp[left+3][right+1],
+                    dp[left+2][right]
+                );
+
+                dp[left+2][right+2] = max(take_left, take_right);        
+            }
+        }
+
+        long long score1 = dp[2][n+1];
 
         long long score2 = total - score1;
 
