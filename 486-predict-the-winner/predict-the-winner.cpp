2a27b2e2958379da0dce5f1376a43aaf6e9579b4
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-02 16:14:27 +0530

    Time: 1 ms (46.53%) | Memory: 10.1 MB (44.98%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,28 +1,30 @@
 class Solution {
 private:
-    long long backtrack(vector<int> &nums, int left, int right) {
+    long long backtrack(vector<int> &nums, int left, int right, vector<vector<int>> &dp) {
         if (left > right) {
             return 0;
         }
+        if (dp[left][right] != -1) return dp[left][right];
 
         long long take_left = nums[left] + min(
-            backtrack(nums, left+2, right),
-            backtrack(nums, left+1, right-1)
+            backtrack(nums, left+2, right, dp),
+            backtrack(nums, left+1, right-1, dp)
         );
 
         long long take_right = nums[right] + min(
-            backtrack(nums, left+1, right-1),
-            backtrack(nums, left, right-2)
+            backtrack(nums, left+1, right-1, dp),
+            backtrack(nums, left, right-2, dp)
         );
 
-        return max(take_left, take_right);
+        return dp[left][right] = max(take_left, take_right);
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n, -1));
         long long total = accumulate(nums.begin(), nums.end(), 0LL);
 
-        long long score1 = backtrack(nums, 0, n-1);
+        long long score1 = backtrack(nums, 0, n-1, dp);
 
         long long score2 = total - score1;
 
