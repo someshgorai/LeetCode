810f10e015bc74af3908e01b8185f508bb33d394
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-05 23:06:36 +0530

    Time: 341 ms (35.17%) | Memory: 334.6 MB (42.07%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:   
-    vector<bool> getSuspicious(int start,  vector<vector<int>> &adj, int n) {
+    vector<bool> getSuspicious(int start,  vector<vector<int>> &adj, int n, vector<int> &indegree) {
         queue<int> q;
         vector<bool> vis(n);
         q.push(start);
@@ -11,59 +11,43 @@ private:
             q.pop();
 
             for (auto neighbor : adj[node]) {
+                indegree[neighbor]--;
                 if (!vis[neighbor]) {
                     q.push(neighbor);
-                    vis[neighbor] = true;
+                    vis[neighbor] = true; 
                 }
             }
         }
 
         return vis;
     }
-    bool isInvokeSus(int start, vector<bool> &vis, vector<bool> &susMethods, vector<vector<int>> &adj, int n) {
-        queue<int> q;
-        q.push(start);
-        vis[start] = true;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            for (auto neighbor : adj[node]) {
-                if (susMethods[neighbor]) return true;
-                if (!vis[neighbor]) {
-                    q.push(neighbor);
-                    vis[neighbor] = true;
-                }
-            }
-        }
-
-        return false;
-    }
 public:
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
         vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
         for (auto edge : invocations) {
             adj[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
         }
 
-        vector<bool> susMethods, vis(n);
-        susMethods = getSuspicious(k, adj, n);
+        vector<bool> susNodes = getSuspicious(k, adj, n, indegree);
 
-        bool invokeSus = false;
+        bool isPresent = true;
         for (int i = 0; i < n; i++) {
-            if (susMethods[i]) continue;
-            if (vis[i])    continue;
-            invokeSus = isInvokeSus(i, vis, susMethods, adj, n);
-            if (invokeSus) break;
+            if (susNodes[i] == true && indegree[i] > 0) {
+                isPresent = false;
+                break;
+            }
         }
 
         vector<int> res;
-        for (int i = 0; i < n; i++) {
-            if (!invokeSus) {
-                if (!susMethods[i]) res.push_back(i);
+        if (isPresent) {
+            for (int i = 0; i < n; i++) {
+                if (!susNodes[i]) res.push_back(i);
             }
-            else {
+        }
+        else {
+            for (int i = 0; i < n; i++) {
                 res.push_back(i);
             }
         }
