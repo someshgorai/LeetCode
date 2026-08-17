90fad7e26a15f002014012703f923a5c0ee24275
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-17 18:49:28 +0530

    Time: 783 ms (21.38%) | Memory: 27.7 MB (33.99%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -8,7 +8,7 @@ private:
         if(dp[left][right] != -1) return dp[left][right];
 
         int maxScore = 0;
-        for (int i = left; i <= right; i++) {
+        for (int i = left; i < right; i++) {
             int leftSum  = prefix[i+1] - prefix[left];
             int rightSum = prefix[right+1] - prefix[i+1];
 
@@ -38,8 +38,33 @@ public:
             prefix[i+1] = stoneValue[i] + prefix[i];
         }
 
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        return backtrack(0, n-1, prefix, dp);
+        for (int left = n-1; left >= 0; left--) {
+            for (int right = left+1; right < n; right++) {
+                int maxScore = 0;
+                for (int i = left; i < right; i++) {
+                    int leftSum  = prefix[i+1] - prefix[left];
+                    int rightSum = prefix[right+1] - prefix[i+1];
+
+                    if (leftSum > rightSum) {
+                        maxScore = max(maxScore, rightSum + dp[i+1][right]);
+                    }
+                    else if (rightSum > leftSum) {
+                        maxScore = max(maxScore, leftSum + dp[left][i]);
+                    }
+                    else {
+                        maxScore = max({
+                            maxScore, 
+                            leftSum + dp[left][i],
+                            rightSum + dp[i+1][right]
+                        });
+                    }
+                }
+                dp[left][right] = maxScore;
+            }
+        }
+
+        return dp[0][n-1];
     }
 };
