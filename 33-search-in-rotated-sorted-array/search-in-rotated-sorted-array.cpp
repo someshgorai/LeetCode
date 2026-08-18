22d18da92c30613afdde69732b15851bed475b46
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-19 01:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (69.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -14,7 +14,7 @@ public:
                 else high = mid-1;
             }
 
-            else if (nums[low] <= nums[mid]) {
+            else  {
                 if (nums[low] <= target && target < nums[mid]) high = mid-1;
                 else low = mid+1; 
             }
