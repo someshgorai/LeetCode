03551bd1b1193366f577096390a62300bbf1f2d2
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-24 23:00:06 +0530

    Time: 96 ms (86.38%) | Memory: 95.2 MB (55.93%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    int backtrack(int idx, int n, vector<int> &stones, vector<int> &dp){
+        if (idx == n-1) return stones[idx+1];
+
+        if (dp[idx] != -1) return dp[idx];
+
+        int take = stones[idx+1] - backtrack(idx+1, n, stones, dp);
+        int notTake = backtrack(idx+1, n, stones, dp);
+
+        return dp[idx] = max(take, notTake);
+    }
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<int> dp(n, 0);
+        vector<int> prefix(n+1, 0);
+
+        for (int i = 0; i < n; i++) {
+            prefix[i+1] = stones[i] + prefix[i];
+        }
+
+        dp[n-1] = prefix[n];
+
+        for (int idx = n-2; idx >= 0; idx--) {
+            int take = prefix[idx+1] - dp[idx+1];
+            int notTake = dp[idx+1];
+            dp[idx] = max(take, notTake);
+        }
+
+        return dp[1];
+    }
+};
