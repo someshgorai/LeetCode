e7845c24d563f16c2ea1d2cb01d8d24c8a813d8b
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-24 18:45:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (47.78%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int n = s.size();
-        vector<bool> prev(n+1, false);
-
-        prev[0] = true;
-
-        for (int idx = n - 1; idx >= 0; idx--) {
-            vector<bool> curr(n+1, false);
-
-            for (int cnt = n - 1; cnt >= 0; cnt--) {
-
-                if (s[idx] == '(') {
-                    curr[cnt] = prev[cnt + 1];
-                }
-                else if (s[idx] == ')') {
-                    if (cnt > 0)
-                        curr[cnt] = prev[cnt - 1];
-                }
-                else {
-                    if (cnt + 1 <= n)
-                        curr[cnt] = prev[cnt + 1];
-
-                    curr[cnt] = curr[cnt] || prev[cnt];
-
-                    if (cnt > 0)
-                        curr[cnt] = curr[cnt] || prev[cnt - 1];
-                }
+        int minRange = 0;
+        int maxRange = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
+                minRange++;
+                maxRange++;
             }
-            swap(curr, prev);
+
+            else if (c == ')') {
+                minRange--;
+                maxRange--;
+            }
+
+            else {
+                minRange--;
+                maxRange++;
+            }
+
+            if (minRange < 0) minRange = 0;
+            if (maxRange < 0) return false;
         }
 
-        return prev[0];
+        return minRange == 0;
     }
 };
