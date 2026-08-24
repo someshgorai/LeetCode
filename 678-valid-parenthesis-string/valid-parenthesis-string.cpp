d5b787d7d6403182a606650119257f8317ea27ad
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-24 18:31:09 +0530

    Time: 3 ms (8.99%) | Memory: 8.2 MB (47.78%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -2,32 +2,35 @@ class Solution {
 public:
     bool checkValidString(string s) {
         int n = s.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
+        vector<bool> prev(n+1, false);
 
-        dp[n][0] = true;
+        prev[0] = true;
 
         for (int idx = n - 1; idx >= 0; idx--) {
+            vector<bool> curr(n+1, false);
+
             for (int cnt = n - 1; cnt >= 0; cnt--) {
 
                 if (s[idx] == '(') {
-                    dp[idx][cnt] = dp[idx + 1][cnt + 1];
+                    curr[cnt] = prev[cnt + 1];
                 }
                 else if (s[idx] == ')') {
                     if (cnt > 0)
-                        dp[idx][cnt] = dp[idx + 1][cnt - 1];
+                        curr[cnt] = prev[cnt - 1];
                 }
                 else {
                     if (cnt + 1 <= n)
-                        dp[idx][cnt] = dp[idx + 1][cnt + 1];
+                        curr[cnt] = prev[cnt + 1];
 
-                    dp[idx][cnt] = dp[idx][cnt] || dp[idx + 1][cnt];
+                    curr[cnt] = curr[cnt] || prev[cnt];
 
                     if (cnt > 0)
-                        dp[idx][cnt] = dp[idx][cnt] || dp[idx + 1][cnt - 1];
+                        curr[cnt] = curr[cnt] || prev[cnt - 1];
                 }
             }
+            swap(curr, prev);
         }
 
-        return dp[0][0];
+        return prev[0];
     }
 };
