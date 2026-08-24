92c3f868ac7a537a2bb31a831b60e62c2762a37b
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-24 18:23:38 +0530

    Time: 5 ms (4.60%) | Memory: 9.4 MB (13.03%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,33 @@
 class Solution {
-private:
-    int backtrack(int idx, int n, int cnt, string s, vector<vector<int>> &dp) {
-        if (idx == n) return cnt == 0;
-        if (cnt < 0) return false;
-        
-        if (dp[idx][cnt] != -1) return dp[idx][cnt];
-
-        if (s[idx] == '(') return dp[idx][cnt] = backtrack(idx+1, n, cnt+1, s, dp);
-        if (s[idx] == ')') return dp[idx][cnt] = backtrack(idx+1, n, cnt-1, s, dp);
-        
-        return dp[idx][cnt] = 
-                backtrack(idx+1, n, cnt+1, s, dp) || 
-                backtrack(idx+1, n, cnt-1, s, dp) ||
-                backtrack(idx+1, n, cnt, s, dp);
-    }
 public:
     bool checkValidString(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return backtrack(0, n, 0, s, dp);
+        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
+
+        dp[n][0] = true;
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int cnt = n - 1; cnt >= 0; cnt--) {
+
+                if (s[idx] == '(') {
+                    dp[idx][cnt] = dp[idx + 1][cnt + 1];
+                }
+                else if (s[idx] == ')') {
+                    if (cnt > 0)
+                        dp[idx][cnt] = dp[idx + 1][cnt - 1];
+                }
+                else {
+                    if (cnt + 1 <= n)
+                        dp[idx][cnt] = dp[idx + 1][cnt + 1];
+
+                    dp[idx][cnt] = dp[idx][cnt] || dp[idx + 1][cnt];
+
+                    if (cnt > 0)
+                        dp[idx][cnt] = dp[idx][cnt] || dp[idx + 1][cnt - 1];
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
