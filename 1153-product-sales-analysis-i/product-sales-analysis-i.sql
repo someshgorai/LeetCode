c955f5e2aeac9e1961fa9863cc927e4300bca1ba
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-08-26 01:01:03 +0530

    Time: 1419 ms (39.53%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT p.product_name, s.year, s.price
-FROM Product AS p JOIN Sales AS s
+FROM Product AS p right JOIN Sales AS s
 ON p.product_id = s.product_id;
