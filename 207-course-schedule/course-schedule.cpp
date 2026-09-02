7abab0399e37f3e4299b188f201d776f5a676d6e
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-09-02 09:41:12 +0530

    Time: 4 ms (64.55%) | Memory: 20.1 MB (24.88%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,24 +1,4 @@
 class Solution {
-private:
-    void toposort(int node, vector<vector<int>> &dirGraph, vector<bool> &vis, int &count) {
-        queue<int> q;
-        q.push(node);
-        vis[node] = true;
-
-        while (!q.empty()) {
-            int node = q.front();
-            count++;
-            q.pop();
-
-            for (int neigh : dirGraph[node]) {
-                if (!vis[neigh]) {
-                    q.push(neigh);
-                    vis[neigh] = true;
-                }
-
-            }
-        }
-    }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> dirGraph(numCourses);
@@ -29,12 +9,10 @@ public:
         }
         
         int count = 0;
-        vector<bool> vis(numCourses, false);
         queue<int> q;
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
                 q.push(i);
-                vis[i] = true;
             }
         }
 
@@ -47,7 +25,6 @@ public:
                 indegree[neigh]--;
                 if (indegree[neigh] == 0){
                     q.push(neigh);
-                    vis[neigh];
                 }
             }
         }
