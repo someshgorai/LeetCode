7d91c5cd4740710b22edd6cd00270381b4e5b94a
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-09-14 11:12:04 +0530

    Time: 4 ms (11.18%) | Memory: 43.5 MB (97.08%) - LeetSync

---
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    int maxKadane(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        int maxSum = -1e9;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            maxSum = max(sum, maxSum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+    int minKadane(vector<int>& nums) {
+        int sum = 0;
+        int n = nums.size();
+        int minSum = 1e9;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            minSum = min(sum, minSum);
+
+            if (sum > 0) {
+                sum = 0;
+            }
+        }
+
+        return minSum;
+    }
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        long long total = accumulate(nums.begin(), nums.end(), 0LL);
+        int cycle = (int)total - minKadane(nums);
+        int nonCycle = maxKadane(nums);
+        if (cycle == 0) return nonCycle;
+        return max(cycle, nonCycle);
+    }
+};
