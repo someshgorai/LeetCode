1426876579b106ebc3f701b76d23e93ca33cbc76
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-09-15 14:46:14 +0530

    Time: 199 ms (39.27%) | Memory: 17.8 MB (61.52%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,36 +1,47 @@
 class Solution {
 private:
-    bool isPalindrome(int start, int end, string& s) {
-        while (start < end) {
-            if (s[start] != s[end]) return false;
-            start++;
-            end--;
-        }
-        return true;
-    }
+    vector<vector<bool>> getPalindromes(string s) {
+        int n = s.size();
+        vector<vector<bool>> isPalindrome(n, vector<bool> (n, false));
 
-    int backtrack(int i, string s, int k, int n, vector<int>& dp) {
-        if (i >= n) return 0;
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i + l <= n; i++) {
+                int j = i + l - 1;
 
-        if (dp[i] != -1) return dp[i];
-
-        int take = 0;
-        for (int j = i + k - 1; j < n; j++) {
-            if (isPalindrome(i, j, s)) {
-                take = max(take, 1 + backtrack(j+1, s, k, n, dp));
+                if (i == j) {
+                    isPalindrome[i][j] = true;
+                }
+                else if (i + 1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]);
+                }
+                else {
+                    isPalindrome[i][j] = (s[i] == s[j]) && isPalindrome[i+1][j-1];
+                }
             }
         }
-        
-        int skip = backtrack(i+1, s, k, n, dp);
 
-        return dp[i] = max(take, skip);
+        return isPalindrome;
     }
-
 public:
     int maxPalindromes(string s, int k) {
         int n = s.size();
         if (k == 1) return n;
-        vector<int> dp(n, -1);
-        return backtrack(0, s, k, n, dp);
+        vector<int> dp(n+1, 0);
+
+        vector<vector<bool>> isPalindrome = getPalindromes(s);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int take = 0;
+            for (int j = i + k - 1; j < n; j++) {
+                if (isPalindrome[i][j]) {
+                    take = max(take, 1 + dp[j+1]);
+                }
+            }
+            
+            int skip = dp[i+1];
+
+            dp[i] = max(take, skip);
+        }
+        return dp[0];
     }
 };
