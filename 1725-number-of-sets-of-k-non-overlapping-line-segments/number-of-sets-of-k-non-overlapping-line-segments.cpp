de07091f8f15dee6cdfaaf55178a4a939a5311b4
SOMESH GORAI <mailme.someshgorai@gmail.com>
2026-09-16 11:02:54 +0530

    Time: 972 ms (5.14%) | Memory: 32.4 MB (41.59%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,23 +1,24 @@
 class Solution {
 private:
     int mod = 1e9+7;
-    int backtrack(int i, int d, int n, int k, vector<vector<int>>& dp) {
-        if (d == k) return 1;
+    int backtrack(int i, int d, int n, vector<vector<int>>& dp) {
+        if (d == 0) return 1;
         if (i >= n) return 0;
 
         if (dp[i][d] != -1) return dp[i][d];
 
         int take = 0;
-        for (int s = i+1; s < n - (k - d - 1); s++) {
-            take = (backtrack(s, d+1, n, k, dp) + take) % mod;
+        for (int s = i+1; s < n - d + 1; s++) {
+            take = (backtrack(s, d-1, n, dp) + take) % mod;
         }
-        int notTake = backtrack(i+1, d, n, k, dp);
+        int notTake = backtrack(i+1, d, n, dp);
 
         return dp[i][d] = (take + notTake) % mod;
     }
 public:
     int numberOfSets(int n, int k) {
-        vector<vector<int>> dp(n, vector<int> (k, -1));
-        return backtrack(0, 0, n, k, dp);
+        vector<vector<int>> dp(n, vector<int> (k+1, -1));
+
+        return backtrack(0, k, n, dp);
     }
 };
